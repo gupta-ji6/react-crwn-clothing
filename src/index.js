793759ccdd9c9b7afb1165f0a158a56d06608586
Ayush Gupta <ayushgupta197@gmail.com>
2019-10-18 10:18:01 +0530

    Added homepage component with scss styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import HomePage from "./homepage.component";
+
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <HomePage />
     </div>
   );
 }
