ee876cd5f9513a08afb584ab964d032a7f69b928
Ayush Gupta <ayushgupta197@gmail.com>
2019-10-21 16:32:13 +0530

    Added Header component and styled it.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import Header from "./components/header/header.component";
 import HomePage from "../src/pages/homepage/homepage.component";
 import ShopPage from "../src/pages/shop/shop.component";
 
@@ -10,6 +11,7 @@ import "./styles.css";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
