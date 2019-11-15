288ecc5beff79f2f48b0d1bc2472de853f96c592
Ayush Gupta <ayushgupta197@gmail.com>
2019-11-15 17:04:44 +0530

    Added FormInput component, giving an error. Fix in next.

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import FormInput from "./../form-input/form-input.component";
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "uyas",
+      password: "wedfwe"
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      email: "",
+      password: ""
+    });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password.</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            handleChange={this.handleChange}
+            value={this.state.password}
+            label="password"
+            required
+          />
+          <input type="submit" value="Sign In" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
