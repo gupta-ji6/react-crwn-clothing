8d9aee461077aea6f2e31a5ce122fd872ee6711f
gupta-ji6 <ayushgupta197@gmail.com>
2020-01-12 16:38:06 +0530

    :floppy_disk: Add redux-persist to save store in localStorage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 import App from "./App";
 import "./index.css";
@@ -12,7 +13,9 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   rootElement
