dd452a2c0721248112f9b13cc1df7290c20f1683
gupta-ji6 <ayushgupta197@gmail.com>
2020-01-12 15:07:38 +0530

    Added checkout page,checkout-item comp & cart selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -25,3 +25,10 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (totalItems, cartItem) => totalItems + cartItem.quantity * cartItem.price,
+    0
+  )
+);
