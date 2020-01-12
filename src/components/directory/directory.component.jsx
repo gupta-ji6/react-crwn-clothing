2d35c85ddb7328b231238d4514d171aefa3f94de
gupta-ji6 <ayushgupta197@gmail.com>
2020-01-13 01:23:18 +0530

    Moved our directory & shop data to own reducer, created respective selector & updated corrsponding components with new ux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,59 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectDirectoryCategories } from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.styles.scss";
 
-class Directory extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats"
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: ""
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: ""
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: ""
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: ""
-        }
-      ]
-    };
-  }
+const Directory = ({ categories }) => (
+  <div className="directory-menu">
+    {categories.map(({ id, ...otherCategoryProps }) => (
+      <MenuItem key={id} {...otherCategoryProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.categories.map(({ id, ...otherCategoryProps }) => (
-          <MenuItem key={id} {...otherCategoryProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  categories: selectDirectoryCategories
+});
+export default connect(mapStateToProps)(Directory);
