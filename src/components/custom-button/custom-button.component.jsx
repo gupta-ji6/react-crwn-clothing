c3a57661e5b65a88de619447356baedfb752ad2e
gupta-ji6 <ayushgupta197@gmail.com>
2020-01-20 10:11:14 +0100

    :nail_care: Added styled-components & changed styles of Homepage,Header & CustomButton

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import "./custom-button.styles.scss";
 
-const CustomButton = ({
-  children,
-  inverted,
-  isGoogleSignIn,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? "inverted" : ""} ${
-      isGoogleSignIn ? "google-sign-in" : ""
-    } custom-button`}
-    {...otherProps}
-  >
-    {children}
-  </button>
+import { CustomButtonContainer } from "./custom-button.styles";
+
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
