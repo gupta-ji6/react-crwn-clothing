335dadda7231839585cb94fc4e1f110dd3651ff3
gupta-ji6 <ayushgupta197@gmail.com>
2020-01-22 23:13:43 +0530

    :nail_care: Styled cart-dropdown & added node-sass

---
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import CustomButton from "../custom-button/custom-button.component";
+
+export const CartDropdownContainer = styled.div`
+  position: absolute;
+  width: 240px;
+  height: 340px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 80px;
+  right: 0;
+  z-index: 5;
+`;
+
+export const CartItemsContainer = styled.div`
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+`;
+
+export const EmptyMessageContainer = styled.span`
+  font-size: 18px;
+  margin: 50px auto;
+`;
+
+export const CartDropdownButton = styled(CustomButton)`
+  margin-top: auto;
+`;
