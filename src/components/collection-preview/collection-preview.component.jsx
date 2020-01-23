18af880bd201abb4fc5e337768110e2bb91745aa
Ayush Gupta <ayushgupta197@gmail.com>
2020-01-23 15:17:34 +0530

    :nail_care: Styled collection-preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import CollectionItem from "../collection-item/collection-item.component";
-import "./collection-preview.styles.scss";
+
+import {
+  CollectionPreviewContainer,
+  Title,
+  Preview
+} from "./collection-preview.styles";
 
 const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+  <CollectionPreviewContainer>
+    <Title>{title.toUpperCase()}</Title>
+    <Preview>
       {items
         .filter((item, index) => index < 4)
         .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </Preview>
+  </CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
