04cfc8a3ad5546268ee833aac0a9e07c78d86c04
gupta-ji6 <ayushgupta197@gmail.com>
2020-05-11 16:31:08 +0530

    :curly_loop: Recrated session persistance with saga

--- src/App.js
+++ src/App.js
@@ -14,29 +14,14 @@ import CheckoutPage from "./pages/checkout/checkout.component.jsx";
 import Header from "./components/header/header.component";
 
 import { selectCurrentUser } from "./redux/user/user.selectors";
+import { checkUserSession } from "./redux/user/user.actions";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    //   if (userAuth) {
-    //     // get userRef from createUserProfileDocument and check whether database has updated at that ref with any new data
-    //     // similar to onAuthstateChanged, we're saying is the snapshot has changed
-    //     const userRef = await createUserProfileDocument(userAuth);
-    //     userRef.onSnapshot(snapShot => {
-    //       // console.log(snapShot) doesn't actually gives us the data. snapShot.data() does that.
-    //       // we haven't stores uId in snapShot.data(). So we get that from snapShot
-    //       setCurrentUser({
-    //         id: snapShot.id,
-    //         ...snapShot.data()
-    //       });
-    //     });
-    //   } else {
-    //     // if userAuth is null, then set state of userAuth to null
-    //     setCurrentUser(userAuth);
-    //   }
-    // });
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -72,4 +57,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  checkUserSession: () => dispatch(checkUserSession()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
