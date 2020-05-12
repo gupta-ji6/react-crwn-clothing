f764c1d1503878cd9d5a5c2d0b0bf21121ec6cb5
gupta-ji6 <ayushgupta197@gmail.com>
2020-05-12 14:14:53 +0530

    :bug: Fixed hover animation in menu-item

--- src/components/menu-item/menu-item.styles.jsx
+++ src/components/menu-item/menu-item.styles.jsx
@@ -1,34 +1,5 @@
 import styled from "styled-components";
 
-export const MenuItemContainer = styled.div`
-  height: ${({ size }) => (size ? "380px" : "240px")};
-  min-width: 30%;
-  overflow: hidden;
-  flex: 1 1 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-  margin: 0 7.5px 15px;
-  overflow: hidden;
-  &:hover {
-    cursor: pointer;
-    & .background-image {
-      transform: scale(1.1);
-      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-    }
-    & .content {
-      opacity: 0.9;
-    }
-  }
-  &:first-child {
-    margin-right: 7.5px;
-  }
-  &:last-child {
-    margin-left: 7.5px;
-  }
-`;
-
 export const BackgroundImageContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -48,6 +19,35 @@ export const ContentContainer = styled.div`
   background-color: white;
   opacity: 0.7;
   position: absolute;
+`;
+
+export const MenuItemContainer = styled.div`
+  height: ${({ size }) => (size ? "380px" : "240px")};
+  min-width: 30%;
+  overflow: hidden;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
+  &:hover {
+    cursor: pointer;
+    & ${BackgroundImageContainer} {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+    & ${ContentContainer} {
+      opacity: 0.9;
+    }
+  }
+  &:first-child {
+    margin-right: 7.5px;
+  }
+  &:last-child {
+    margin-left: 7.5px;
+  }
 `;
 
 export const ContentTitle = styled.span`
